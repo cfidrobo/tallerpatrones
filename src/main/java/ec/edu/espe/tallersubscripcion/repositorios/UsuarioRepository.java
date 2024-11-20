509eb0999f14de67da586b5dc2e221b6074d68e4
Cristian Fernando Idrobo Montalvo <105193365+cfidrobo@users.noreply.github.com>
2024-11-20 17:37:42 -0500

    Delete src directory

--- src/main/java/ec/edu/espe/tallersubscripcion/repositorios/UsuarioRepository.java
+++
@@ -1,6 +0,0 @@
-package ec.edu.espe.tallersubscripcion.repositorios;
-import ec.edu.espe.tallersubscripcion.entidades.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
